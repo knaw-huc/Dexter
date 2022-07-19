4abefd913487d595b39b8214b5ec684e7a992cee
Sebastiaan van Daalen <sebastiaan.van.daalen@di.huc.knaw.nl>
2022-07-19 12:02:57 +0200

    Added comment about non-null assertion in createRoot

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,9 @@ import { createRoot } from "react-dom/client"
 import { App } from "./app"
 
 const container = document.getElementById("container")
+
+// "createRoot(container!) is recommended by React docs: https://reactjs.org/blog/2022/03/08/react-18-upgrade-guide.html. See line: "const root = createRoot(container); // createRoot(container!) if you use TypeScript"".
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const root = createRoot(container!)
 
 root.render(<App />)
