5387e7546697d1c19181126e1cfce29d3582dc59
HDJ <hayco@users.noreply.github.com>
2022-10-05 14:35:21 +0200

    Add POC test that Auth header passes all proxies

--- backend/server/src/main/kotlin/nl/knaw/huc/dexter/resources/AboutResource.kt
+++ backend/server/src/main/kotlin/nl/knaw/huc/dexter/resources/AboutResource.kt
@@ -5,8 +5,10 @@ import io.swagger.v3.oas.annotations.Operation
 import nl.knaw.huc.dexter.api.AboutInfo
 import nl.knaw.huc.dexter.api.ResourcePaths
 import nl.knaw.huc.dexter.config.DexterConfiguration
+import org.slf4j.LoggerFactory
 import java.time.Instant
 import javax.ws.rs.GET
+import javax.ws.rs.HeaderParam
 import javax.ws.rs.Path
 import javax.ws.rs.Produces
 import javax.ws.rs.core.MediaType
@@ -14,20 +16,24 @@ import javax.ws.rs.core.MediaType
 @Path(ResourcePaths.ABOUT)
 @Produces(MediaType.APPLICATION_JSON)
 class AboutResource(
-        configuration: DexterConfiguration,
-        appName: String,
-        version: String
+    configuration: DexterConfiguration,
+    appName: String,
+    version: String
 ) {
+    val log = LoggerFactory.getLogger(AboutResource::class.java)
+
     private val about = AboutInfo(
-            appName = appName,
-            version = version,
-            startedAt = Instant.now().toString(),
-            baseURI = configuration.externalBaseUrl
+        appName = appName,
+        version = version,
+        startedAt = Instant.now().toString(),
+        baseURI = configuration.externalBaseUrl
     )
 
     @Operation(description = "Get server info")
     @Timed
     @GET
-    fun getAboutInfo() = about
-
+    fun getAboutInfo(@HeaderParam("Authorization") auth: String): AboutInfo {
+        log.info("auth: $auth")
+        return about
+    }
 }
