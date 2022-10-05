7a5f5b00fb92071d55ca5f83e83ed2938e6d3fb9
Sebastiaan van Daalen <sebastiaan.van.daalen@di.huc.knaw.nl>
2022-10-05 10:09:59 +0200

    Build proper frontend app Docker

--- frontend/src/Components/Home.tsx
+++ frontend/src/Components/Home.tsx
@@ -1,11 +1,17 @@
 import React from "react"
 import styled from "styled-components"
 import { Link, Outlet } from "react-router-dom"
+import Config from "../Config"
 
 const NavStyled = styled.nav`
     border-bottom: "solid 1px";
     padding-bottom: 1rem;
 `
+
+fetch(Config.BACKEND_HOST)
+    .then(function(response) {
+        console.log(response)
+    })
 
 export const Home = () => {
     return (
