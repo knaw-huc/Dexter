d8bcb54dda4254941e4092367e100f4537c2116d
HDJ <hayco@users.noreply.github.com>
2022-10-08 20:09:09 +0200

    Use Flyway migrations to set up db

--- backend/server/src/main/kotlin/nl/knaw/huc/dexter/DexterApplication.kt
+++ backend/server/src/main/kotlin/nl/knaw/huc/dexter/DexterApplication.kt
@@ -13,12 +13,13 @@ import io.dropwizard.setup.Environment
 import nl.knaw.huc.dexter.api.Constants
 import nl.knaw.huc.dexter.api.Constants.APP_NAME
 import nl.knaw.huc.dexter.config.DexterConfiguration
+import nl.knaw.huc.dexter.config.FlywayConfiguration
 import nl.knaw.huc.dexter.resources.AboutResource
+import org.flywaydb.core.Flyway
 import org.jdbi.v3.core.Jdbi
 import org.jdbi.v3.postgres.PostgresPlugin
 import org.jdbi.v3.sqlobject.SqlObjectPlugin
 import org.slf4j.LoggerFactory
-import java.util.UUID
 
 class DexterApplication : Application<DexterConfiguration>() {
     private val log = LoggerFactory.getLogger(javaClass)
@@ -27,7 +28,7 @@ class DexterApplication : Application<DexterConfiguration>() {
 
     override fun initialize(bootstrap: Bootstrap<DexterConfiguration>) {
         bootstrap.configurationSourceProvider = SubstitutingSourceProvider(
-                bootstrap.configurationSourceProvider, EnvironmentVariableSubstitutor()
+            bootstrap.configurationSourceProvider, EnvironmentVariableSubstitutor()
         )
         bootstrap.addBundle(getSwaggerBundle())
         bootstrap.addBundle(JdbiExceptionsBundle())
@@ -35,17 +36,17 @@ class DexterApplication : Application<DexterConfiguration>() {
 
     private fun getSwaggerBundle() = object : SwaggerBundle<DexterConfiguration>() {
         override fun getSwaggerBundleConfiguration(configuration: DexterConfiguration): SwaggerBundleConfiguration =
-                configuration.swaggerBundleConfiguration
+            configuration.swaggerBundleConfiguration
     }
 
     override fun run(configuration: DexterConfiguration, environment: Environment) {
         log.info(
-                "DEX_ environment variables:\n\n" +
-                        Constants.EnvironmentVariable.values()
-                                .joinToString("\n") { ev ->
-                                    "  ${ev.name}:\t${System.getenv(ev.name) ?: "(not set, using default)"}"
-                                } +
-                        "\n"
+            "DEX_ environment variables:\n\n" +
+                    Constants.EnvironmentVariable.values()
+                        .joinToString("\n") { ev ->
+                            "  ${ev.name}:\t${System.getenv(ev.name) ?: "(not set, using default)"}"
+                        } +
+                    "\n"
         )
 
         migrateDatabase(configuration.dataSourceFactory, configuration.flyway)
@@ -58,6 +59,30 @@ class DexterApplication : Application<DexterConfiguration>() {
         val appVersion = javaClass.getPackage().implementationVersion
         environment.jersey().apply {
             register(AboutResource(configuration, name, appVersion))
+        }
+    }
+
+    private fun migrateDatabase(dsf: DataSourceFactory, conf: FlywayConfiguration) {
+        log.info("conf.baselineOnMigrate: ${conf.baselineOnMigrate}")
+        log.info("conf.baselineVersion: ${conf.baselineVersion}")
+        val flywayConfig = Flyway.configure()
+            .dataSource(dsf.url, dsf.user, dsf.password)
+            .baselineVersion(conf.baselineVersion)
+            .baselineOnMigrate(conf.baselineOnMigrate)
+            .cleanDisabled(conf.cleanDisabled)
+            .locations(*conf.locations) // unpack primitive array using spread ('*') operator
+        val flyway = Flyway(flywayConfig)
+        val ms = flyway.migrate()
+        if (ms.targetSchemaVersion != null || ms.migrationsExecuted > 0) {
+            val from = ms.initialSchemaVersion
+            val to = ms.targetSchemaVersion
+            val result = if (ms.success) "OK" else "FAILED"
+            log.info("Migration from: $from to $to -> $result")
+            log.info("Number of migrations executed: ${ms.migrationsExecuted}")
+            ms.migrations.forEachIndexed { i, m ->
+                log.info("migration[$i]: desc=\"${m.description}\", cat: ${m.category}, type: ${m.type}, ver: ${m.version}")
+                log.info(" + ${m.filepath} ran in ${m.executionTime}ms")
+            }
         }
     }
 
