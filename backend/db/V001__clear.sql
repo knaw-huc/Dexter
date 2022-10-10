f351f47b93dde690b26381fc81f3313f4b074683
HDJ <hayco@users.noreply.github.com>
2022-10-10 12:12:09 +0200

    clean up more

--- backend/db/V001__clear.sql
+++ backend/db/V001__clear.sql
@@ -3,4 +3,8 @@
 -- Note Drop in reverse order of creation to ensure constraints are dropped
 -- before the tables that those constraints depend on, are removed.
 
-drop table if exists users;
+drop table if exists corpora cascade;
+drop table if exists sources cascade;
+drop table if exists users cascade;
+drop function if exists trigger_set_updated_at cascade;
+drop type if exists access_type cascade;
