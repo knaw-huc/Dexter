83bd162dafc99ae392b2723d3bcab5be7c62b831
HDJ <hayco@users.noreply.github.com>
2022-10-10 13:28:27 +0200

    Fix, clean and expand tables/indexes

--- backend/db/V002__init.sql
+++ backend/db/V002__init.sql
@@ -12,10 +12,19 @@ begin
 end;
 $$ language plpgsql;
 
+create table keywords (
+    id serial primary key,
+    val text not null unique
+);
+
+create table languages (
+  id smallserial primary key,
+  name text not null unique
+);
+
 create table users (
     id uuid primary key,
-    login text not null unique,
-    name text not null
+    name text not null unique
 );
 
 create table sources (
@@ -28,9 +37,8 @@ create table sources (
   location text,
   earliest date,
   latest date,
-  language text, -- multiple languages
   notes text,
-  created_by uuid not null,
+  created_by uuid not null references users (id),
   created_at timestamp not null default now(),
   updated_at timestamp not null default now()
 );
@@ -40,9 +48,23 @@ create trigger set_sources_updated_at
     for each row
     execute procedure trigger_set_updated_at();
 
+create table sources_keywords (
+    source_id uuid references sources (id),
+    key_id serial references keywords (id),
+    unique (source_id, key_id)
+);
+create index on sources_keywords (key_id, source_id);
+
+create table sources_languages (
+    source_id uuid references sources (id),
+    lang_id smallserial references languages (id),
+    unique (source_id, lang_id)
+);
+create index on sources_languages (lang_id, source_id);
+
 create table corpora (
     id uuid primary key,
-    parent_id uuid,
+    parent_id uuid references corpora (id),
     title text not null,
     description text not null,
     rights text not null,
@@ -50,16 +72,21 @@ create table corpora (
     location text,
     earliest date,
     latest date,
-    language text,
     contributor text,
     notes text,
-    created_by uuid not null,
+    created_by uuid not null references users (id),
     created_at timestamp not null default now(),
-    updated_at timestamp not null default now(),
-    constraint corpora_parent_id_fkey foreign key (parent_id) references corpora (id)
+    updated_at timestamp not null default now()
 );
 
 create trigger set_corpora_updated_at
     before update on corpora
     for each row
-    execute procedure trigger_set_updated_at()
+    execute procedure trigger_set_updated_at();
+
+create table corpora_languages (
+    corpus_id uuid references corpora (id),
+    lang_id smallserial references languages (id),
+    unique (corpus_id, lang_id)
+);
+create index on corpora_languages (lang_id, corpus_id);
