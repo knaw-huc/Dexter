82a7d28e456004ddc42fe8946a4f6201ac3cd2dc
HDJ <hayco@users.noreply.github.com>
2022-10-14 13:14:01 +0200

    Simplify and make Dao calls transaction safe
    * extract common code to do Dao call
    * make transaction safe
    * handle diagnostics in one place
    * simplify what needs to be done per resource

--- backend/server/src/main/kotlin/nl/knaw/huc/dexter/resources/CorporaResource.kt
+++ backend/server/src/main/kotlin/nl/knaw/huc/dexter/resources/CorporaResource.kt
@@ -11,7 +11,7 @@ import nl.knaw.huc.dexter.api.ResultKeyword
 import nl.knaw.huc.dexter.auth.DexterUser
 import nl.knaw.huc.dexter.db.CorporaDao
 import nl.knaw.huc.dexter.db.UsersDao
-import nl.knaw.huc.dexter.helpers.PsqlConstraintChecker.Companion.checkConstraintViolations
+import nl.knaw.huc.dexter.helpers.PsqlDiagnosticsHelper.Companion.diagnoseViolations
 import org.jdbi.v3.core.Jdbi
 import org.slf4j.LoggerFactory
 import java.util.*
@@ -37,64 +37,70 @@ class CorporaResource(private val jdbi: Jdbi) {
     fun createCorpus(formCorpus: FormCorpus, @Auth user: DexterUser): ResultCorpus {
         log.info("createCorpus[${user.name}]: formCorpus=[$formCorpus]")
         val createdBy = users().findByName(user.name) ?: throw NotFoundException("Unknown user: $user")
-        return checkConstraintViolations { corpora().insert(formCorpus, createdBy.id) }
+        return diagnoseViolations { corpora().insert(formCorpus, createdBy.id) }
     }
 
     @PUT
     @Consumes(APPLICATION_JSON)
     @Path(ID_PATH)
     fun updateCorpus(
-        @PathParam(ID_PARAM) corpusId: UUID,
+        @PathParam(ID_PARAM) id: UUID,
         formCorpus: FormCorpus,
         @Auth user: DexterUser
-    ): ResultCorpus {
-        log.info("updateCorpus[${user.name}]: corpusId=$corpusId, formCorpus=$formCorpus")
-        corpora().find(corpusId)?.let {
-            return checkConstraintViolations { corpora().update(corpusId, formCorpus) }
+    ): ResultCorpus =
+        onExistingCorpus(id) { dao, corpus ->
+            log.info("updateCorpus[${user.name}]: corpusId=$corpus.id, formCorpus=$formCorpus")
+            dao.update(id, formCorpus)
         }
-        corpusNotFound(corpusId)
-    }
 
     @DELETE
     @Path(ID_PATH)
-    fun deleteCorpus(@PathParam(ID_PARAM) corpusId: UUID, @Auth user: DexterUser): Response {
-        log.info("deleteCorpus[${user.name}]: corpusId=$corpusId")
-        corpora().find(corpusId)?.let {
-            log.warn("$user deleting: $it")
-            checkConstraintViolations { corpora().delete(corpusId) }
-            return Response.noContent().build()
+    fun deleteCorpus(@PathParam(ID_PARAM) id: UUID, @Auth user: DexterUser): Response =
+        onExistingCorpus(id) { dao, corpus ->
+            log.warn("deleteCorpus[${user.name}] deleting: $corpus")
+            dao.delete(id)
+            Response.noContent().build()
         }
-        corpusNotFound(corpusId)
-    }
 
     @GET
     @Path("$ID_PATH/$KEYWORDS/v1")
-    fun getKeywordsV1(@PathParam(ID_PARAM) corpusId: UUID) =
-        corpora().find(corpusId)?.let {
-            corpora().getKeywords(corpusId)
-        } ?: corpusNotFound(corpusId)
+    fun getKeywordsV1(@PathParam(ID_PARAM) id: UUID) =
+        onExistingCorpus(id) { dao, corpus ->
+            dao.getKeywords(corpus.id)
+        }
 
     @POST
     @Path("$ID_PATH/$KEYWORDS")
-    fun addKeyword(@PathParam(ID_PARAM) corpusId: UUID, keywordId: String): List<ResultKeyword> {
-        log.info("addKeyword: corpusId=$corpusId, keywordId=$keywordId")
-        corpora().find(corpusId)?.let {
-            corpora().addKeyword(corpusId, keywordId.toInt())
-            return corpora().getKeywords(corpusId)
-        } ?: corpusNotFound(corpusId)
-    }
+    fun addKeyword(@PathParam(ID_PARAM) id: UUID, keywordId: String): List<ResultKeyword> =
+        onExistingCorpus(id) { dao, corpus ->
+            log.info("addKeyword: corpusId=$corpus.id, keywordId=$keywordId")
+            dao.addKeyword(corpus.id, keywordId.toInt())
+            dao.getKeywords(corpus.id)
+        }
 
     @DELETE
     @Path("$ID_PATH/$KEYWORDS/{keywordId}")
     fun deleteKeyword(
-        @PathParam(ID_PARAM) corpusId: UUID,
+        @PathParam(ID_PARAM) id: UUID,
         @PathParam("keywordId") keywordId: Int
-    ) : List<ResultKeyword> {
-        log.info("deleteKeyword: corpusId=$corpusId, keywordId=$keywordId")
-        corpora().find(corpusId)?.let {
-            corpora().deleteKeyword(corpusId, keywordId)
-            return corpora().getKeywords(corpusId)
-        }?: corpusNotFound(corpusId)
+    ): List<ResultKeyword> =
+        onExistingCorpus(id) { dao, corpus ->
+            log.info("deleteKeyword: corpusId=$corpus.id, keywordId=$keywordId")
+            dao.deleteKeyword(corpus.id, keywordId)
+            dao.getKeywords(corpus.id)
+        }
+
+    private fun <R> onExistingCorpus(corpusId: UUID, block: DaoBlock<CorporaDao, ResultCorpus, R>): R =
+        jdbi.inTransaction<R, Exception> { handle ->
+            handle.attach(CorporaDao::class.java).let { dao ->
+                dao.find(corpusId)?.let { corpus ->
+                    diagnoseViolations{ block.execute(dao, corpus) }
+                } ?: corpusNotFound(corpusId)
+            }
+        }
+
+    private fun interface DaoBlock<D, I, R> {
+        fun execute(dao: D, it: I): R
     }
 
     private fun corpusNotFound(corpusId: UUID): Nothing = throw NotFoundException("Corpus not found: $corpusId")
