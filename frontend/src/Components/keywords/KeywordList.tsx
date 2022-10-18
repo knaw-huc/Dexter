c439f4dda976b0010d2672fb998bf2405311600f
Sebastiaan van Daalen <sebastiaan.van.daalen@di.huc.knaw.nl>
2022-10-18 15:38:42 +0200

    Added delete keyword functionality

--- frontend/src/Components/keywords/KeywordList.tsx
+++ frontend/src/Components/keywords/KeywordList.tsx
@@ -1,25 +1,44 @@
 import React from "react"
 import { Keywords } from "../../Model/DexterModel"
-import { getKeywords } from "../API"
+import { deleteKeyword, getKeywords } from "../API"
 import { NewKeywords } from "./NewKeywords"
+import DeleteIcon from "@mui/icons-material/Delete"
+import styled from "@emotion/styled"
+
+const DeleteIconStyled = styled(DeleteIcon)`
+    margin-left: 5px;
+    &:hover {
+        cursor: pointer;
+        color: gray;
+    }
+`
 
 export const KeywordList = () => {
     const [keywords, setKeywords] = React.useState<Keywords[]>()
+
+    React.useEffect(() => {
+        doGetKeywords()
+    }, [])
 
     const doGetKeywords = async () => {
         const kw = await getKeywords()
         setKeywords(kw)
     }
 
-    React.useEffect(() => {
-        doGetKeywords()
-    }, [])
+    const handleDelete = async (id: number) => {
+        const warning = window.confirm("Are you sure you wish to delete this keyword?")
+
+        if (warning === false) return
+
+        await deleteKeyword(id)
+        await doGetKeywords()
+    }
 
     return (
         <>
             <NewKeywords setKeywords={setKeywords} />
             {keywords && keywords.map((keyword: Keywords, index: number) => (
-                <div key={index}>{keyword.id} {keyword.val}</div>
+                <div key={index}>{keyword.id} {keyword.val} {<DeleteIconStyled color="error" onClick={() => handleDelete(keyword.id)}>Delete corpus</DeleteIconStyled>}</div>
             ))}
         </>
     )
