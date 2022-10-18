359beee1d0864055e601c1dd27ccb193bc86c702
HDJ <hayco@users.noreply.github.com>
2022-10-18 14:05:54 +0200

    Add support to upload iso-639-3 code set
    Redistribution is not part of the terms of use,
    so point user to download location and allow simple
    curl --upload-file to seed iso-639-3 table.

--- backend/server/src/main/kotlin/nl/knaw/huc/dexter/resources/LanguagesResource.kt
+++ backend/server/src/main/kotlin/nl/knaw/huc/dexter/resources/LanguagesResource.kt
@@ -7,14 +7,65 @@ import nl.knaw.huc.dexter.api.ResourcePaths.LANGUAGES
 import nl.knaw.huc.dexter.api.ResultLanguage
 import nl.knaw.huc.dexter.db.LanguagesDao
 import org.jdbi.v3.core.Jdbi
+import org.slf4j.LoggerFactory
+import javax.validation.constraints.NotNull
 import javax.ws.rs.*
 import javax.ws.rs.core.MediaType.APPLICATION_JSON
+import javax.ws.rs.core.MediaType.TEXT_PLAIN
+
+private const val ISO_639_URL = "https://iso639-3.sil.org/code_tables/download_tables"
 
 @Path(LANGUAGES)
 @Produces(APPLICATION_JSON)
 class LanguagesResource(private val jdbi: Jdbi) {
+    private val log = LoggerFactory.getLogger(javaClass)
+
     @GET
-    fun list() = languages().list()
+    fun list() = mapOf(
+        "source" to "ISO 639-3 Downloads from: $ISO_639_URL",
+        "termsOfUse" to "$ISO_639_URL#termsofuse",
+        "languages" to languages().list()
+            .map { it.refName }
+            .ifEmpty {
+                throw NotFoundException(
+                    "Please seed with ISO 639-3 Code Set (UTF-8) file. " +
+                            "Get it from $ISO_639_URL and upload using, e.g., " +
+                            "'curl --upload-file iso-639-3.tab <base_uri>/languages'"
+                )
+            })
+
+    @PUT
+    @Consumes(TEXT_PLAIN)
+    fun seed(@NotNull contents: String) =
+        contents.lineSequence().also {
+            it.first().run {
+                if (this != "Id\tPart2B\tPart2T\tPart1\tScope\tLanguage_Type\tRef_Name\tComment") {
+                    log.warn("Invalid iso-639-3 code file, header was [$this].")
+                    throw BadRequestException(
+                        "Invalid iso-639-3 Code Set file, please get it from: $ISO_639_URL"
+                    )
+                }
+            }
+        }.drop(1) // Ignore CSV header line
+            .map { line ->
+                val cols = line.split('\t')
+                ResultLanguage(
+                    id = cols[0],
+                    part2b = cols[1],
+                    part2t = cols[2],
+                    part1 = cols[3],
+                    scope = cols[4][0],
+                    type = cols[5][0],
+                    refName = cols[6],
+                    comment = cols[7]
+                ).also {
+                    log.trace("Adding [${it.id}] -> [${it.refName}]")
+                }
+            }
+            .let {
+                languages().seed(it.asIterable())
+                mapOf("count" to languages().list().size)
+            }
 
     @GET
     @Path(ID_PATH)
@@ -31,10 +82,8 @@ class LanguagesResource(private val jdbi: Jdbi) {
             3 -> dao.findByLength3(key)
             else -> emptyList()
         }.ifEmpty {
-            if (key.length > 2)
-                dao.findByRefName("%$key%")
-            else
-                emptyList()
+            if (key.length > 2) dao.findByRefName("%$key%")
+            else emptyList()
         }.sortedWith { o1, o2 ->
             when (o1.type) {
                 o2.type -> o1.refName.compareTo(o2.refName)
