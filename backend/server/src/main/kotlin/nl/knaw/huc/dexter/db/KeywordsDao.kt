abd835f338de553a23f6bba8752680aff3831a86
HDJ <hayco@users.noreply.github.com>
2022-10-19 15:49:03 +0200

    Make keywords uniform to other data types
    Just having a String instead of full-fledged (JSON) object made things unnecessarily complex in front end handling.
    Let's just use the same construct everywhere.

--- backend/server/src/main/kotlin/nl/knaw/huc/dexter/db/KeywordsDao.kt
+++ backend/server/src/main/kotlin/nl/knaw/huc/dexter/db/KeywordsDao.kt
@@ -2,6 +2,7 @@ package nl.knaw.huc.dexter.db
 
 import nl.knaw.huc.dexter.api.FormKeyword
 import nl.knaw.huc.dexter.api.ResultKeyword
+import org.jdbi.v3.sqlobject.kotlin.BindKotlin
 import org.jdbi.v3.sqlobject.statement.SqlQuery
 import org.jdbi.v3.sqlobject.statement.SqlUpdate
 
@@ -12,11 +13,11 @@ interface KeywordsDao {
     @SqlQuery("select * from keywords where id = :id")
     fun find(id: Int): ResultKeyword?
 
-    @SqlQuery("insert into keywords (val) values (:keyword) returning *")
-    fun insert(keyword: FormKeyword): ResultKeyword
+    @SqlQuery("insert into keywords (val) values (:val) returning *")
+    fun insert(@BindKotlin keyword: FormKeyword): ResultKeyword
 
-    @SqlQuery("update keywords set val = :keyword where id = :id returning *")
-    fun update(id: Int, keyword: FormKeyword): ResultKeyword
+    @SqlQuery("update keywords set val = :val where id = :id returning *")
+    fun update(id: Int, @BindKotlin keyword: FormKeyword): ResultKeyword
 
     @SqlUpdate("delete from keywords where id = :id")
     fun delete(id: Int)
