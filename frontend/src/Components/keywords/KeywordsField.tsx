742c81e5ffe33cc6693eb7e9f989fc8e345bfc99
Sebastiaan van Daalen <sebastiaan.van.daalen@di.huc.knaw.nl>
2022-10-19 19:09:47 +0200

    Keywords autocomplete without debounce

--- frontend/src/Components/keywords/KeywordsField.tsx
+++ frontend/src/Components/keywords/KeywordsField.tsx
@@ -2,22 +2,39 @@ import React from "react"
 import { Control, Controller } from "react-hook-form"
 import { Autocomplete } from "@mui/material"
 import { TextField } from "@mui/material"
-import { getKeywords } from "../API"
+import { getKeywords, getKeywordsAutocomplete } from "../API"
 import { Collections, Sources, Keywords } from "../../Model/DexterModel"
 import parse from "autosuggest-highlight/parse"
 import match from "autosuggest-highlight/match"
 
 export const KeywordsField = ({ control }: { control: Control<Keywords | Collections | Sources> }) => {
-    const [keywords, setKeywords] = React.useState<Keywords[]>()
+    const [keywords, setKeywords] = React.useState<Keywords[]>([])
+    const [inputValue, setInputValue] = React.useState("")
 
-    const doGetKeywords = async () => {
-        const kw = await getKeywords()
-        setKeywords(kw)
+    // const doGetKeywords = async () => {
+    //     const kw = await getKeywords()
+    //     setKeywords(kw)
+    // }
+
+    // React.useEffect(() => {
+    //     doGetKeywords()
+    // }, [])
+
+
+    async function autoComplete(input: string) {
+        console.log(input)
+        const result = await getKeywordsAutocomplete(input)
+        console.log(result)
+        setKeywords(result)
+        return result
     }
 
+
     React.useEffect(() => {
-        doGetKeywords()
-    }, [])
+        if (inputValue.length > 2) {
+            autoComplete(inputValue)
+        }
+    }, [inputValue])
 
     return (
         <div>
@@ -26,10 +43,14 @@ export const KeywordsField = ({ control }: { control: Control<Keywords | Collect
                 name={"val"}
                 render={({ field: { onChange, value } }) => (
                     <Autocomplete
+                        inputValue={inputValue}
+                        open={inputValue.length > 2}
+                        onInputChange={async (event, value) => { setInputValue(value) }}
                         multiple={true}
                         id="keywords-autocomplete"
                         options={keywords}
                         getOptionLabel={(keyword: Keywords) => keyword.val}
+                        filterOptions={(x) => x}
                         renderInput={(params) => (
                             <TextField
                                 {...params}
