2c8b3ed479745424f35f1ae407f807aede5ef42a
Sebastiaan van Daalen <sebastiaan.van.daalen@di.huc.knaw.nl>
2022-11-01 13:40:14 +0100

    Made LanguagesContent component more generic

--- frontend/src/Components/languages/LanguagesContent.tsx
+++ frontend/src/Components/languages/LanguagesContent.tsx
@@ -3,9 +3,11 @@ import { ServerLanguage } from "../../Model/DexterModel"
 import DeleteIcon from "@mui/icons-material/Delete"
 import styled from "@emotion/styled"
 import { red } from "@mui/material/colors"
+import { getLanguagesCorpora, getLanguagesSources } from "../API"
 
 type LanguagesContentProps = {
-    languages: ServerLanguage[],
+    sourceId?: string,
+    corpusId?: string,
     onDelete: (language: ServerLanguage) => Promise<void>
 }
 
@@ -19,9 +21,37 @@ const DeleteIconStyled = styled(DeleteIcon)`
 `
 
 export const LanguagesContent = (props: LanguagesContentProps) => {
+    const [languages, setLanguages] = React.useState<ServerLanguage[]>(null)
+
+    const doGetSourceLanguages = async (sourceId: string) => {
+        const langs = await getLanguagesSources(sourceId)
+        setLanguages(langs)
+        console.log(langs)
+    }
+
+    const doGetCorpusLanguages = async (corpusId: string) => {
+        const langs = await getLanguagesCorpora(corpusId)
+        setLanguages(langs)
+        console.log(langs)
+    }
+
+    React.useEffect(() => {
+        if (props.sourceId) {
+            doGetSourceLanguages(props.sourceId)
+        }
+
+        if (props.corpusId) {
+            doGetCorpusLanguages(props.corpusId)
+        }
+    }, [props.corpusId, props.sourceId])
+
     return (
         <>
-            {props.languages.map((language, index) => {
+            {/* {props.languages.map((language, index) => {
+                return <p key={index}>{language.refName} {<DeleteIconStyled onClick={() => props.onDelete(language)} />}</p>
+            })} */}
+
+            {languages && languages.map((language, index) => {
                 return <p key={index}>{language.refName} {<DeleteIconStyled onClick={() => props.onDelete(language)} />}</p>
             })}
         </>
