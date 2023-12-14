31e5073eff83580478977c80464419fdfe56b4cf
BasLee <basleenknegt@gmail.com>
2023-12-14 15:54:55 +0100

    Show menu login icon when not logged in

--- frontend/src/Components/Header.tsx
+++ frontend/src/Components/Header.tsx
@@ -10,6 +10,7 @@ import Avatar from "@mui/material/Avatar"
 import Button from "@mui/material/Button"
 import MenuItem from "@mui/material/MenuItem"
 import ScienceIcon from "@mui/icons-material/Science"
+import LoginIcon from "@mui/icons-material/Login"
 import {useContext} from "react"
 import {userContext} from "../State/User/userContext"
 import {useNavigate} from "react-router-dom"
@@ -18,99 +19,111 @@ const pages = ["corpora", "sources"]
 const settings = ["account"]
 
 export default function Header() {
-    const username = useContext(userContext).userState.username
     const navigate = useNavigate()
-    const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null)
+    const username = useContext(userContext).userState.username
+
+    return <AppBar position="static">
+        <Container maxWidth="xl">
+            <Toolbar disableGutters>
+                <ScienceIcon
+                    onClick={() => window.location.href = "/"}
+                    sx={{display: {xs: "none", md: "flex"}, mr: 0.5}}
+                />
+                <Typography
+                    onClick={() => window.location.href = "/"}
+                    variant="h6"
+                    noWrap
+                    component="a"
+                    href="#app-bar-with-responsive-menu"
+                    sx={{
+                        mr: 2,
+                        display: {xs: "none", md: "flex"},
+                        fontFamily: "monospace",
+                        fontWeight: 700,
+                        letterSpacing: ".3rem",
+                        color: "inherit",
+                        textDecoration: "none",
+                    }}
+                >
+                    DEXTER
+                </Typography>
+                <Box sx={{flexGrow: 1, display: {xs: "none", md: "flex"}}}>
+                    {pages.map((page) => (
+                        <Button
+                            key={page}
+                            onClick={() => navigate("/" + page)}
+                            sx={{my: 2, color: "white", display: "block"}}
+                        >
+                            {page}
+                        </Button>
+                    ))}
+                </Box>
+                {username
+                    ? <UserMenuAvatar/>
+                    : <LoginAvatar/>
+                }
+            </Toolbar>
+        </Container>
+    </AppBar>
+}
+
+function LoginAvatar() {
+    const navigate = useNavigate()
+
+    return <Box sx={{flexGrow: 0}}>
+        <IconButton onClick={() => navigate("/login")} sx={{p: 0}}>
+            <Avatar>
+                <LoginIcon/>
+            </Avatar>
+        </IconButton>
+    </Box>
+}
+
+function UserMenuAvatar() {
+    const username = useContext(userContext).userState.username
+
     const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null)
-
-    const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
-        setAnchorElNav(event.currentTarget)
-    }
-    const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
-        setAnchorElUser(event.currentTarget)
-    }
-
-    const handleCloseNavMenu = () => {
-        setAnchorElNav(null)
-    }
-
     const handleCloseUserMenu = () => {
         setAnchorElUser(null)
     }
 
-    return (
-        <AppBar position="static">
-            <Container maxWidth="xl">
-                <Toolbar disableGutters>
-                    <ScienceIcon
-                        onClick={() => window.location.href = "/"}
-                        sx={{display: {xs: "none", md: "flex"}, mr: 0.5}}
-                    />
-                    <Typography
-                        onClick={() => window.location.href = "/"}
-                        variant="h6"
-                        noWrap
-                        component="a"
-                        href="#app-bar-with-responsive-menu"
-                        sx={{
-                            mr: 2,
-                            display: {xs: "none", md: "flex"},
-                            fontFamily: "monospace",
-                            fontWeight: 700,
-                            letterSpacing: ".3rem",
-                            color: "inherit",
-                            textDecoration: "none",
-                        }}
-                    >
-                        DEXTER
-                    </Typography>
-                    <Box sx={{flexGrow: 1, display: {xs: "none", md: "flex"}}}>
-                        {pages.map((page) => (
-                            <Button
-                                key={page}
-                                onClick={() => navigate("/" + page)}
-                                sx={{my: 2, color: "white", display: "block"}}
-                            >
-                                {page}
-                            </Button>
-                        ))}
-                    </Box>
+    const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorElUser(event.currentTarget)
+    }
 
-                    <Box sx={{flexGrow: 0}}>
-                        <IconButton onClick={handleOpenUserMenu} sx={{p: 0}}>
-                            <Avatar
-                                alt={username
-                                    ? username[0].toUpperCase()
-                                    : "Anonymous"
-                                }
-                                src="/static/images/avatar/2.jpg"
-                            />
-                        </IconButton>
-                        <Menu
-                            sx={{mt: "45px"}}
-                            id="menu-appbar"
-                            anchorEl={anchorElUser}
-                            anchorOrigin={{
-                                vertical: "top",
-                                horizontal: "right",
-                            }}
-                            keepMounted
-                            transformOrigin={{
-                                vertical: "top",
-                                horizontal: "right",
-                            }}
-                            open={Boolean(anchorElUser)}
-                            onClose={handleCloseUserMenu}
-                        >
-                            {settings.map((setting) => (
-                                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                                    <Typography textAlign="center">{setting}</Typography>
-                                </MenuItem>
-                            ))}
-                        </Menu>
-                    </Box>
-                </Toolbar>
-            </Container>
-        </AppBar>
-    )
+
+
+    return <Box sx={{flexGrow: 0}}>
+        <IconButton onClick={handleOpenUserMenu} sx={{p: 0}}>
+            <Avatar
+                alt={username
+                    ? username[0].toUpperCase()
+                    : "Anonymous"
+                }
+                src="/static/images/avatar/2.jpg"
+            />
+        </IconButton>
+        <Menu
+            sx={{mt: "45px"}}
+            id="menu-appbar"
+            anchorEl={anchorElUser}
+            anchorOrigin={{
+                vertical: "top",
+                horizontal: "right",
+            }}
+            keepMounted
+            transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+            }}
+            open={Boolean(anchorElUser)}
+            onClose={handleCloseUserMenu}
+        >
+            {settings.map((setting) => (
+                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                    <Typography textAlign="center">{setting}</Typography>
+                </MenuItem>
+            ))}
+        </Menu>
+    </Box>
 }
