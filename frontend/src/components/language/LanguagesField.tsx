01b9e2d054adb5e112d599e6eb612ca7a60d76f1
BasLee <basleenknegt@gmail.com>
2024-03-04 13:52:19 +0100

    Use MultiAutocomplete when selecting languages

--- frontend/src/components/language/LanguagesField.tsx
+++ frontend/src/components/language/LanguagesField.tsx
@@ -1,98 +1,36 @@
-import Autocomplete from '@mui/material/Autocomplete';
-import Chip from '@mui/material/Chip';
-import TextField from '@mui/material/TextField';
-import match from 'autosuggest-highlight/match';
-import parse from 'autosuggest-highlight/parse';
 import React from 'react';
 import { ResultLanguage } from '../../model/DexterModel';
-import { useDebounce } from '../../utils/useDebounce';
 import { getLanguagesAutocomplete } from '../../utils/API';
-import { TextFieldProps } from '@mui/material';
+import { MultiAutocomplete } from '../common/MultiAutocomplete';
 
-interface LanguagesFieldProps {
+export type LanguagesFieldProps = {
   selected: ResultLanguage[];
   onChangeSelected: (selected: ResultLanguage[]) => void;
-}
+};
 
-export const LanguagesField = (props: LanguagesFieldProps) => {
-  const [options, setOptions] = React.useState<ResultLanguage[]>([]);
-  const [inputValue, setInputValue] = React.useState('');
-  const [loading, setLoading] = React.useState(false);
-  const debouncedInput = useDebounce<string>(inputValue, 250);
+export function LanguagesField(props: LanguagesFieldProps) {
+  async function handleAutocomplete(input: string) {
+    return await getLanguagesAutocomplete(input);
+  }
 
-  React.useEffect(() => {
-    if (debouncedInput.length > 0) {
-      autoComplete(debouncedInput);
-      setLoading(true);
-    }
-  }, [debouncedInput]);
+  function handleAddSelected(o: ResultLanguage) {
+    return props.onChangeSelected([...props.selected, o]);
+  }
 
-  const autoComplete = async (input: string) => {
-    const result = await getLanguagesAutocomplete(input);
-    setOptions(result);
-    setLoading(false);
-    return result;
-  };
-  const handleDeleteLanguage = (tag: ResultLanguage) => {
-    const newSelected = props.selected.filter(l => l.id !== tag.id);
-    props.onChangeSelected(newSelected);
-  };
-
-  function renderInputField(params: TextFieldProps): JSX.Element {
-    return <TextField {...params} value={inputValue} size={'medium'} />;
+  function handleRemoveSelected(o: ResultLanguage) {
+    return props.onChangeSelected(props.selected.filter(s => s.id !== o.id));
   }
 
   return (
-    <Autocomplete
-      inputValue={inputValue}
-      open={debouncedInput.length > 0}
-      onInputChange={async (event, value) => {
-        setInputValue(value);
-      }}
-      multiple={true}
-      loading={loading}
-      id="languages-autocomplete"
-      options={options}
-      getOptionLabel={(language: ResultLanguage) => language.refName}
-      filterOptions={x => x}
-      isOptionEqualToValue={(option, value) => option.refName === value.refName}
-      value={props.selected}
-      renderInput={renderInputField}
-      renderTags={(tagValue, getTagProps) =>
-        tagValue.map((language, index) => (
-          <Chip
-            label={language.refName}
-            key={index}
-            {...getTagProps({ index })}
-            onDelete={() => handleDeleteLanguage(language)}
-          />
-        ))
-      }
-      onChange={(_, data) => {
-        props.onChangeSelected(data as ResultLanguage[]);
-      }}
-      renderOption={(props, option, { inputValue }) => {
-        const label = option.refName + ' [' + option.id + ']';
-        const matches = match(label, inputValue, { insideWords: true });
-        const parts = parse(label, matches);
-
-        return (
-          <li {...props}>
-            <div>
-              {parts.map((part, index) => (
-                <span
-                  key={index}
-                  style={{
-                    fontWeight: part.highlight ? 700 : 400,
-                  }}
-                >
-                  {part.text}
-                </span>
-              ))}
-            </div>
-          </li>
-        );
-      }}
+    <MultiAutocomplete<ResultLanguage>
+      placeholder="Search and select languages"
+      selected={props.selected}
+      onAutocomplete={handleAutocomplete}
+      toStringLabel={o => o.refName}
+      isOptionEqualToValue={(option, value) => option.id === value.id}
+      onAddSelected={handleAddSelected}
+      onRemoveSelected={handleRemoveSelected}
+      allowCreatingNew={false}
     />
   );
-};
+}
